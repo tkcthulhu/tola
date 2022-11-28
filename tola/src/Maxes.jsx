e636553397bf66345c547a38566ff8181f20967c
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-11-28 23:41:59 +0000

    tabs work properly and maxes display

--- tola/src/Maxes.jsx
+++ tola/src/Maxes.jsx
@@ -1,7 +1,21 @@
 function Maxes(props) {
     let maxes = [...props.users.maxes]
 
-    console.log(maxes)
+    let max_view = []
+
+    for (const max of maxes) {
+
+        console.log(max.exercise)
+        max_view.push(
+            <li className="list-group-item">{max.exercise}<br/><p className="tab">{max.reps} Rep Max: {max.weight}</p></li>
+        )
+    }
+
+    return (
+        <ul className="list-group list-group-flush">
+            {max_view}
+        </ul>
+    )
 }
 
 export default Maxes
--- tola/src/Navbar.jsx
+++ tola/src/Navbar.jsx
@@ -6,17 +6,49 @@ function Navbar(props) {
 
   const [page, setPage] = useState('Home');
 
+  function activeTab(id) {
+
+    const tabs = ['Home', 'Training', 'Maxes', 'Programs']
+
+    for (const tab of tabs) {
+      document.getElementById(tab).classList.remove('active')
+    }
+
+    document.getElementById(id).classList.add('active')
+
+    console.log('here')
+
+  }
+
   return(
     <>
       <ul className="nav nav-tabs">
         <li className="nav-item">
-          <a id='Home' className="nav-link active" aria-current="page" onClick={() => setPage('Home')}>Home</a>
+          <a 
+            id='Home' 
+            className="nav-link active" 
+            aria-current="page" 
+            onClick={() => {
+              setPage('Home');
+              activeTab('Home')
+              }}>
+              Home
+            </a>
         </li>
         <li className="nav-item">
           <a id='Training' className="nav-link" aria-current="page">Training</a>
         </li>
         <li className="nav-item">
-          <a id='Maxes' className="nav-link" aria-current="page" onClick={() => setPage('Maxes')}>Maxes</a>
+          <a 
+            id='Maxes' 
+            className="nav-link" 
+            aria-current="page" 
+            onClick={() => {
+              setPage('Maxes')
+              activeTab('Maxes');
+            }}>
+              Maxes
+            </a>
         </li>
         <li className="nav-item">
           <a id='Programs' className="nav-link" aria-current="page">Programs</a>
@@ -26,6 +58,7 @@ function Navbar(props) {
       {/* { page === 'Training' && <Training />} */}
       { page === 'Maxes' && <Maxes users={props.users} />}
       {/* { page === 'Programs' && <Programs />} */}
+      <activeTab id={page}/>
     </>
   )
 }
