9a394b972c274f58d50eb3621f08598dd0036013
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-12-01 22:40:21 +0000

    user gym selections now works for switching SMASH SMASH

--- tola/src/components/Profile/UpdateGym.jsx
+++ tola/src/components/Profile/UpdateGym.jsx
@@ -7,71 +7,90 @@ import React from 'react';
 function UpdateGym(props) {
 
     function useForceUpdate() {
-      const [value, setValue] = React.useState(0);
-      let v = value
-      return () => setValue((value) => v + 1);
+        const [value, setValue] = React.useState(0);
+        let v = value
+        return () => setValue((value) => v + 1);
     }
 
     const gymList = useRef(null)
-
     const [gyms, setGyms] = useState([])
+    const [userGyms, setUserGyms] = useState([])
+
 
     useEffect(() => {
         axios.get(`https://8000-tkcthulhu-tolaapi-g6ziba3two5.ws-us77.gitpod.io/api/gymAPI/`)
-        .then((resp) => setGyms(resp.data));
+            .then((resp) => setGyms(resp.data));
     }, [])
-
-    console.log(gyms)
 
     let forceUpdate = useForceUpdate()
 
-    const weight = React.useRef(null);
-
-  
     function deactivateOldGym(currentGym) {
-      
-      axios.put(`https://8000-tkcthulhu-tolaapi-g6ziba3two5.ws-us77.gitpod.io/api/usergymAPI/${currentGym}/`, {
-          "id": currentGym,
-          "active": false
-        }
-        )
-        
+
+        axios.put(`https://8000-tkcthulhu-tolaapi-g6ziba3two5.ws-us77.gitpod.io/api/usergymAPI/${currentGym}/`, {
+            "id": currentGym,
+            "active": false
+        })
+
     }
 
-    async function postGym(user, gym) {
-      
-          await axios.post(`https://8000-tkcthulhu-tolaapi-g6ziba3two5.ws-us77.gitpod.io/api/usergymAPI/`, {
-              "user": user,
-              "gym": gym,
-              "active": true,
-              "date_joined": `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}`,
-          })
-          .catch((err) => console.log(err));
-          
-          if (props.users.gym) {
-              deactivateOldGym(props.users.gym.user_gym_id)
-            }
+    useEffect(() => {
+        axios.get(`https://8000-tkcthulhu-tolaapi-g6ziba3two5.ws-us77.gitpod.io/api/usergymAPI/`)
+            .then((resp) => setUserGyms(resp.data));
+    }, [])
+
+    function postGym(user, gym) {
+
+        if (props.users.gym){
+            deactivateOldGym(props.users.gym.user_gym_id)
         }
 
+        let currentGym = gym
+
+        for (const userGym of userGyms) {
+            if (userGym.user == user && userGym.gym == gym) {
+                currentGym=userGym.id
+                console.log(userGym.id)
+            } 
+        }
+        
+        if (currentGym === gym) {
+            console.log('new')
+            axios
+                .post(`https://8000-tkcthulhu-tolaapi-g6ziba3two5.ws-us77.gitpod.io/api/usergymAPI/`, {
+                    "user": user,
+                    "gym": gym,
+                    "active": true,
+                })
+            .catch((err) => console.log(err + 'this one officer'));
+        } else {
+            console.log('update')
+            axios
+                .put(`https://8000-tkcthulhu-tolaapi-g6ziba3two5.ws-us77.gitpod.io/api/usergymAPI/${currentGym}/`, {
+                    "active": true,
+            })
+        }
+
+    }
+
     const handleClose = () => {
-      props.setShow(false)
+        props.setShow(false)
     };
 
     function gymListBuild() {
-    
+
         let gymList = []
-    
+
         for (const gym of gyms) {
             gymList.push(
-                <option 
-                    value= {gym.id}
-                    key= {gym.id}
+                <option
+                    value={gym.id}
+                    key={gym.id}
                 >
-                  {gym.name}
+                    {gym.name}
                 </option>
             )
         }
-        return(
+        return (
             gymList
         )
     }
@@ -79,37 +98,37 @@ function UpdateGym(props) {
     return (
 
         <Modal
-          show={props.show}
-          onHide={handleClose}
-          backdrop="static"
-          keyboard={false}
+            show={props.show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
         >
-          <Modal.Header closeButton>
-            <Modal.Title>Select new gym</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            What gym would you like to join?
-            <select 
-              name="list" 
-              id="listSelect"
-              className='inputItem'
-              ref={gymList}
-            >
-              {gymListBuild()}
-            </select>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button 
-              variant="danger"
-              onClick={() => {
-                postGym(props.users.id, gymList.current.value);
-                handleClose();
-                forceUpdate();
-              }}
-            >
-              Set New Gym
-            </Button>
-          </Modal.Footer>
+            <Modal.Header closeButton>
+                <Modal.Title>Select new gym</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                What gym would you like to join?
+                <select
+                    name="list"
+                    id="listSelect"
+                    className='inputItem'
+                    ref={gymList}
+                >
+                    {gymListBuild()}
+                </select>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    variant="danger"
+                    onClick={() => {
+                        postGym(props.users.id, gymList.current.value);
+                        handleClose();
+                        forceUpdate();
+                    }}
+                >
+                    Set New Gym
+                </Button>
+            </Modal.Footer>
         </Modal>
 
     );
