4ba6afa8646de08b70e7f2727cfd4c4e7c2d6971
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-12-18 01:55:12 +0000

    planning for creation of program feature

--- tola/src/components/CreateProgram.jsx
+++ tola/src/components/CreateProgram.jsx
@@ -3,6 +3,7 @@ function CreateProgram(props) {
         <>
             <h1>Create a new program</h1>
             <h1>Add a session</h1>
+            {/* What program to add, what week to put, session number, (exercise, num_of_sets, %, max exercise) */}
         </>
     )
 }
