520eb6577d92f1efc72420331ec52eaf98f45b76
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-12-19 15:31:36 +0000

    create new program input added

--- tola/src/components/CreateProgram.jsx
+++ tola/src/components/CreateProgram.jsx
@@ -1,9 +1,15 @@
 function CreateProgram(props) {
     return (
-        <>
-            <h1>Create a new program</h1>
+        <div className="container">
+            <div className="row">
+                <h1>Create a new program</h1>
+            </div>
+            <div className="row">
+                <input type="text" placeholder="New program name" />
+            </div>
+
             <h1>Add a session</h1>
             {/* What program to add, what week to put, session number, (exercise, num_of_sets, %, max exercise) */}
-        </>
+        </div>
     )
 }
